65e71779c4d1001d0bbf13f261459a94afbab166
dainmuanynok-cmyk <dainmuanynok@gmail.com>
2025-11-25 09:14:28 +0700

    finally commit

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
 
 export default function App() {
@@ -25,10 +25,80 @@ export default function App() {
     { id: 20, name: "Aston Villa"},
   ];
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.topic}>แนะนำอาหารไทย</Text>
+        <Text style={styles.discription}>( 20 รายการ และราคาโดยประมาณ )</Text>
+      </View>
+      <ScrollView contentContainerStyle={styles.body}>
+        {products.map((p) => (
+          <View style={styles.card} key={p.id}>
+            <Text style={styles.index}># {p.id}</Text>
+            <Text style={styles.title}>{p.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: "#5f2ce0ff",
+  },
+  topic: {
+    fontSize: 25,
+    fontWeight: "700",
+    color: "#fff",
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    marginBottom: 8,
+    textAlign: "center",
+  },
+  discription: {
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 20,
+    color: "#fff",
+  },
+  body: {
+    backgroundColor: "#8d67eeff",
+    paddingTop: 50,
+    paddingBottom: 50,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: 30,
+    justifyContent: "center",
+    minHeight: "100%",
+  },
+  card: {
+    backgroundColor: "#f8f8f8",
+    borderRadius: 8,
+    padding: 16,
+    width: 300,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  index: {
+    fontSize: 18,
+    fontWeight: "800",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "700",
+    marginBottom: 10,
+    color: "#3a3a3dff",
+  },
+  price: {
+    fontSize: 16,
+    color: "#68686bff",
+  },
+});
